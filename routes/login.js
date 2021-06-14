ff19d173134bb03c787841bc298b96d5e498d2f7
AntiMelee <tmskiti@outlook.jp>
2021-06-14 14:06:15 +0900

    login周りのUXを向上 次はhomepageの一部表示処理とmypageへの偏移

--- routes/login.js
+++ routes/login.js
@@ -3,48 +3,45 @@ const NodeMailer = require('nodemailer');
 const bcrypt = require('bcrypt');
 const Users = require('../models/user');
 const uuid = require('uuid');
+const { Op } = require('sequelize');
 const { render } = require('ejs');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('login', {title: 'ログイン'});
+  res.render('login', { title: 'ログイン' });
 });
 
 router.post('/', function (req, res, next) {
-  if(/^[0-9a-zA-Z]{3,15}$/.test(req.body.userid)) {
-    console.log("投げられている")
-    Users.findOne({
-      where: {
-        userid: req.body.userid,
-      }
-    }).then((userData) => {
-      if(!userData){
-        console.log("未登録ユーザー");
-        res.render('login',{title:"ログイン"});
-        //TODO  未登録で弾かれた場合のユーザーへの通知
-      }else{
-        bcrypt.compare(req.body.pass, userData.pass).then(function(result, err) {
-          if(result){
-            let session = uuid.v4();
-            Users.update(
-              { session: session },
-              { where: { systemid: userData.systemid}}
-            )
-            req.session.authentication = session;
-            req.session.user = userData.systemid;
-            console.log(req.session.authentication);
-            console.log(req.session.user);
-            res.redirect('/homepage');
-            console.log("ログイン完了")
-          } else {
-            console.log("パスワードが間違っています");
-          }
-        })
-      }
-    })
-  }else{
-    console.log("入力に不備があります");
-  }
+  console.log("投げられている")
+  Users.findOne({
+    where: {
+      [Op.or]: [{ userid: req.body.useridOraddress }, { address: req.body.useridOraddress }]
+    }
+  }).then((userData) => {
+    if (!userData) {
+      console.log("未登録ユーザー");
+      res.redirect('/');
+      //TODO  未登録で弾かれた場合のユーザーへのメッセージ
+    } else {
+      bcrypt.compare(req.body.pass, userData.pass).then(function (result, err) {
+        if (result) {
+          let session = uuid.v4();
+          Users.update(
+            { session: session },
+            { where: { systemid: userData.systemid } }
+          )
+          req.session.authentication = session;
+          req.session.user = userData.systemid;
+          console.log(req.session.authentication);
+          console.log(req.session.user);
+          res.redirect('/homepage');
+          console.log("ログイン完了")
+        } else {
+          console.log("パスワードが間違っています");
+        }
+      })
+    }
+  })
 });
 
 module.exports = router;
